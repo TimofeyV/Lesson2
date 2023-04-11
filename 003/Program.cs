c40bcd021b76099507515d49b1cef04fc718aeba
TimofeyV <vazhenin.t@gmail.com>
2023-04-11 20:52:41 +0300

    work3

---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+if (number < 0 || number > 7) Console.WriteLine("Вы ввели некоректное число");
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine("Это выходной");
+}
+else
+{
+    Console.WriteLine("Это будний день");
+}
