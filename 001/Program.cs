d04147b5e8eead75d45dae7e4ebf8cd75876e269
TimofeyV <vazhenin.t@gmail.com>
2023-04-11 20:20:32 +0300

    work1

---
+++ 001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите 3-х значное число: ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+int result = number / 10;
+result = result % 10;
+Console.Write("Вторая цифра этого числа: ");
+Console.WriteLine(result);
