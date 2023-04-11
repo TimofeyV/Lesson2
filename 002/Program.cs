99f09c10e761b4df9f7953346d1c147451619ecb
TimofeyV <vazhenin.t@gmail.com>
2023-04-11 20:45:17 +0300

    work2

---
+++ 002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+int result = number;
+int count = 0;
+
+if (-100 < number && number < 100) Console.WriteLine("B числе нет 3-ей цифры");
+else
+{
+    while(number > 999)
+    {
+        number /= 10;
+    }
+    Console.WriteLine(number % 10);
+}
